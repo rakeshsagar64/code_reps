2b9deb5c6802c9ffe3a4d2d800a311eea10cec09
Rakesh Sagar <Developer@mail.com>
2026-09-07 11:27:25 +0530

    Added all courses and made question water tight

--- src/curriculum/index.js
+++ src/curriculum/index.js
@@ -3,10 +3,14 @@ import regexM1 from './courses/regex/module-1.json';
 import regexM2 from './courses/regex/module-2.json';
 import regexM3 from './courses/regex/module-3.json';
 import regexM4 from './courses/regex/module-4.json';
+import regexM5 from './courses/regex/module-5.json';
+import regexM6 from './courses/regex/module-6.json';
+import regexM7 from './courses/regex/module-7.json';
+import regexM8 from './courses/regex/module-8.json';
 
 export const regexCourse = {
   ...regexCourseMeta,
-  modules: [regexM1, regexM2, regexM3, regexM4]
+  modules: [regexM1, regexM2, regexM3, regexM4, regexM5, regexM6, regexM7, regexM8]
 };
 
 export const courses = {
